d19a6435868958dcc46e94467541a0dd9aa93304
Jethro Carr <jethro.carr@jethrocarr.com>
2015-04-10 18:18:48 +1200

    Added pluginsync-like functionality

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class pupistry {
+
+  # There is no support for pluginsync when running masterless, meaning that you
+  # puppet facts won't work. The following does the same thing as plugin sync when
+  # running masterless.
+  # TODO: Do we need logic to determine if masterless or not?
+  file { $::settings::libdir:
+    ensure  => directory,
+    source  => 'puppet:///plugins',
+    recurse => true,
+    purge   => true,
+    backup  => false,
+    noop    => false
+  }
+
+}
+
+# vi:smartindent:tabstop=2:shiftwidth=2:expandtab:
