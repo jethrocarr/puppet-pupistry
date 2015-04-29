827bc805200c29786477d12e7e933572ad73ccdf
Jethro Carr <jethro.carr@jethrocarr.com>
2015-04-29 22:12:48 +1200

    Defining variables for class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,14 @@
 # Shell for including task-specific files & parameters
-class pupistry inherits pupistry::params {
+class pupistry (
+  $puppet_pluginsync  = $pupistry::params::puppet_pluginsync,
+  $install_pupistry   = $pupistry::params::install_pupistry,
+  $install_daemon     = $pupistry::params::install_daemon,
+  $init_system        = $pupistry::params::init_system,
+  ) inherits pupistry::params {
+
   include pupistry::install
   include pupistry::puppet
+
 }
 
 # vi:smartindent:tabstop=2:shiftwidth=2:expandtab:
