72df93279c6469127eaea699f31ddcfc0a7ec817
Jethro Carr <jethro.carr@jethrocarr.com>
2015-06-07 21:50:45 +1200

    Migrated init detection to https://github.com/jethrocarr/puppet-initfact

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,61 +16,12 @@ class pupistry::params {
   # artifacts and doing Puppet runs.
   $install_daemon     = true
 
+  # We use the jethrocarr/initfact module to identify the init system for us
+  $init_system = $::initsystem
 
-  # Determine what init system is running on this system.
-  #
-  # TODO: Might be worth writing a proper fact for this and getting it pushed
-  #       upstream into Puppet?
-
-  $init_system = $::osfamily ? {
-
-    'RedHat' => $::operatingsystem ? {
-      'Amazon' => $::operatingsystemmajrelease ? {
-        '2014'  => 'sysvinit',
-        '2015'  => 'sysvinit',
-        default => 'sysvinit',
-      },
-      default => $::operatingsystemmajrelease ? {
-        # Covers CentOS & RHEL
-        '5'     => 'sysvinit',
-        '6'     => 'sysvinit',
-        '7'     => 'systemd',
-        default => 'systemd',
-      },
-    },
-    
-    'FreeBSD' => $::operatingsystemmajrelease ? {
-      '9'     => 'bsdinit',
-      '10'    => 'bsdinit',
-      default => 'bsdinit',
-    },
-
-    'Debian' => $::operatingsystem ? {
-      'Ubuntu' => $::operatingsystemmajrelease ? {
-        '12.04' => 'upstart',
-        '14.04' => 'upstart',
-        '14.10' => 'upstart',
-        '15.04' => 'systemd',
-        default => 'systemd',   # All future Ubuntu versions will be systemd
-      },
-      'Debian' => $::operatingsystemmajrelease ? {
-        '7'     => 'sysvinit',
-        '8'     => 'systemd',
-        default => 'systemd',   # All future Debian versions will be systemd
-      },
-      # See comments below re defaults.
-      default => 'sysvinit',
-    },
-
-    'Darwin' => 'unsupported',
-
-    # Default for any unknown system is sysvinit since most distros inc systemd
-    # using still support sysvinit scripts. In future, this default will change
-    # to systemd, so if you're using sysvinit and you're not listed in the above
-    # logic, please send in a pull request to avoid it suddenly changing.
-    default   => 'sysvinit',
+  if (!$init_system) {
+    fail('Install the jethrocarr/initfact module to provide identification of the init system being used. Required to make this module work.')
   }
-
 
 }
 
